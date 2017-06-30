5fc5847029d9b49a294230a5e47372c05eec6de2
Marian Tietz <marian.tietz@ottogroup.com>
2017-06-30 17:49:09 +0200

    bnorm default is off

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
-parser.add_argument('--batchnorm', choices=['off','decoder','encoder','all'])
+parser.add_argument('--batchnorm', choices=['off','decoder','encoder','all'], default='off')
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
